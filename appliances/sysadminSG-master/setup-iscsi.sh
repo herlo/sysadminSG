ccdc3b4a446c4c1874abfe9d9de0ec5be32666c6
Jon Stanley <jonstanley@gmail.com>
2012-01-14 15:14:50 -0500

    oops, need a size

--- appliances/sysadminSG-master/setup-iscsi.sh
+++ appliances/sysadminSG-master/setup-iscsi.sh
@@ -2,7 +2,7 @@
 
 mkdir /iscsi-targets
 for file in $(seq 1 50); do
-   dd if=/dev/zero of=/iscsi-targets/target-$file
+   dd if=/dev/zero of=/iscsi-targets/target-$file bs=1M count=10
 done
 chkconfig tgtd on
 
